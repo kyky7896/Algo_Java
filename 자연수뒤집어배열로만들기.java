a74b8a52330ca910fd90d425d37e19769d9e03bb
kyky7896 <kyky7896@gmail.com>
2022-11-25 14:03:28 +0900

    221125

---
+++ 자연수뒤집어배열로만들기.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+  public int[] solution(long n) {
+      int[] answer = {};
+      String numbers=String.valueOf(n);
+      String[] number=numbers.split("");
+      System.out.println(Arrays.toString(number));
+      return answer;
+  }
+} 
